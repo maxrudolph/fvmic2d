2deb48961903f3900630a5986cec0a21ee9c4378
Maxwell Rudolph <max@seismo.berkeley.edu>
2015-05-29 11:08:49 -0700

    changed redblue colormap

--- post_processing/redblue.m
+++ post_processing/redblue.m
@@ -1,7 +1,7 @@
 function [ cmap ] = redblue()
 %make a colormap that goes from red(+) to blue(-) via white
 scale = linspace(0,1,32)';
-scale = scale.^2;
+% scale = scale.^2;
 cmap = zeros(64,3);
 cmap(1:32,:) =  (scale) * [1 1 1];
 cmap(33:64,:) =  (scale(end:-1:1)) * [1 1 1];
